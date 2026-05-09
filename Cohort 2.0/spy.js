b2389aeaad2475fa83a52c734dfd6bf806210ca4
Tanishq Tawar <tanishqamittawar@gmail.com>
2026-05-09 12:15:29 +0530

    Spy

--- Cohort 2.0/spy.js
+++ Cohort 2.0/spy.js
@@ -7,3 +7,25 @@ ground.addEventListener('mousemove', function(e){
     ground.style.setProperty('--x', x + 'px')
     ground.style.setProperty('--y', y + 'px')
 })
+
+
+let h1 =document.querySelector('h1')
+let text = h1.textContent
+let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+let iteration = 0
+
+function getrandom(){ {
+   
+   let random = text.split('').map(function(char,index){
+     if(iteration >= index) {return text[index]}
+         return alphabet.split('')[ Math.floor(Math.random() * 52)]
+}).join("")
+iteration += 0.5
+h1.innerText = random
+
+}}
+
+h1.addEventListener('mouseover', function(){
+
+setInterval(getrandom ,100)})
